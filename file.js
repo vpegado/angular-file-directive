a8fb9d8c2d2d773355891fc871d621c90aea8e16
Victor Pegado <victor@pegado.se>
2014-05-18 21:43:19 +0200

    Update file to support multiple files

--- file.js
+++ file.js
@@ -1,19 +1,41 @@
-angular.module('file', [])
-    .directive("file", [function () {
-        return {
-            scope: {
-                file: "="
-            },
-            link: function (scope, element, attributes) {
-                element.bind("change", function (changeEvent) {
-                    var reader = new FileReader();
-                    reader.onload = function (loadEvent) {
-                        scope.$apply(function () {
-                            scope.file = loadEvent.target.result;
-                        });
-                    }
-                    reader.readAsText(changeEvent.target.files[0]);
+'use strict';
+
+angular.module('adminApp')
+  .directive('file', [function () {
+    return {
+      scope: {
+        file: '='
+      },
+      link: function (scope, elem) {
+        elem.bind('change', function (changeEvent) {
+          var files = [];
+          angular.forEach(changeEvent.target.files, function (file) {
+            var isText = file.type.indexOf('text/') === 0;
+            var reader = new FileReader();
+            reader.file = file;
+            reader.onload = function (e) {
+              var data = e.target.result;
+              if (!isText) {
+                data = btoa(data);
+              }
+              files.push({
+                type: file.type,
+                name: file.name,
+                body: data
+              });
+              if (files.length === changeEvent.target.files.length) {
+                scope.$apply(function () {
+                  scope.file = files;
                 });
+              }
+            };
+            if (isText) {
+              reader.readAsText(file);
+            } else {
+              reader.readAsBinaryString(file);
             }
-        }
-    }]);
+          });
+        });
+      }
+    };
+  }]);
