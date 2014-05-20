ec19fa8d104addae4ebdd7dc7b670c52999e140c
Victor Pegado <victor@pegado.se>
2014-05-20 16:48:01 +0200

    Add comments

--- file.js
+++ file.js
@@ -7,28 +7,35 @@ angular.module('file', [])
         file: '='
       },
       link: function (scope, elem) {
+        // Listen for change in the input element
         elem.bind('change', function (changeEvent) {
           var files = [];
+          // For Each targeted file
           angular.forEach(changeEvent.target.files, function (file) {
+            // Check if file type is 'text'
             var isText = file.type.indexOf('text/') === 0;
             var reader = new FileReader();
             reader.file = file;
             reader.onload = function (e) {
               var data = e.target.result;
+              // Base64 Encode file of type other than 'text'
               if (!isText) {
                 data = btoa(data);
               }
+              // Compile data from file
               files.push({
                 type: file.type,
                 name: file.name,
                 body: data
               });
+              // Apply new values to $scope when the last file is done.
               if (files.length === changeEvent.target.files.length) {
                 scope.$apply(function () {
                   scope.file = files;
                 });
               }
             };
+            // Read file as 'text' or 'binary string'
             if (isText) {
               reader.readAsText(file);
             } else {
